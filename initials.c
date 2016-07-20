839eef807d589cc93c695ff70ca968d0a20b6a02
jwass91 <jwass91@gmail.com>
2016-07-20 18:58:29 -0400

    added homework files

---
+++ initials.c
@@ -0,0 +1,12 @@
+/*Jared Wasserman*/
+/*This program prints initials made out of #*/
+
+#include <stdio.h>
+
+int main(){
+	
+	printf("#####  #             #\n    #   #           #\n    #   #           #\n    #    #    #    #\n    #    #   # #   #\n#   #     #  # #  # \n#  #      # #   # #  \n ##        #     #   \n ");
+
+	return 0;
+
+}
